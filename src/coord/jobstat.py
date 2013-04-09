fee08627779eb29e0609174631f9901de4157228
liyin <liyinhgqw@gmail.com>
2013-04-09 16:00:35 -0400

    Add jobstat.

---
+++ src/coord/jobstat.py
@@ -0,0 +1,85 @@
+'''
+Created on Apr 9, 2013
+
+@author: stud
+'''
+
+import coord.common
+import coord.slave
+import threading
+from functools import partial
+
+class JobStat(object):
+  '''
+  Statistics of each job calculated and recorded by slaves.
+  '''
+
+  def __init__(self, jobname, interval, slave):
+    self.jobname = jobname
+    self.interval = interval
+    self.slave = slave
+    self.lock = threading.Lock
+    
+    # avg stats
+    self.runtime = -1
+    self.backuprate = -1;
+    
+    self._st = -1
+    self._et = -1
+    self._elapse = -1
+    self._backup = -1
+    
+    self.start_update()
+    
+  def has_runtime_stat(self):
+    return self.runtime > 0
+  
+  def has_backuprate_stat(self):
+    return self.backuprate > 0
+    
+  def start(self):
+    with self.lock:
+      self._st = coord.common.curtime()
+  
+  def finish(self):
+    if self._st > 0:
+      self._et = coord.common.curtime()
+      self._elapse = self._et - self._st
+      if self._elapse > 0:
+        # for runtime
+        if self.runtime < 0:
+          self.runtime = self._elapse
+        else:
+          self.runtime = self.runtime * 0.6 + self._elapse * 0.4
+        
+          cur_backup = self.slave._get_unfinished_input_totalsize(self.jobname)
+          self.backuprate = cur_backup - self._backup
+          self._backup = cur_backup
+    with self.lock:
+      self._st = -1
+    
+  def update_backuprate(self):
+    # for backuprate
+      cur_backup = self.slave._get_unfinished_input_totalsize(self.jobname)
+      self.backuprate = cur_backup - self._backup
+    
+  def update(self):
+    if self.slave._checknclear_finished(self.jobname):
+      self.finish()
+      
+    self.update_backuprate()
+    
+    t = threading.Timer(self.interval, self.update)
+    t.start()
+  
+  def start_update(self):
+    t = threading.Timer(self.interval, self.update)
+    t.start()
+    
+    
+    
+    
+    
+  
+  
+        
