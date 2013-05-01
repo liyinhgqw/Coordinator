58fb4b77e4312519db0294d487f79ead4f09c52f
liyin <liyinhgqw@gmail.com>
2013-05-01 17:19:21 -0400

    Implement part of dfs.

--- src/coord/common.py
+++ src/coord/common.py
@@ -8,6 +8,8 @@ import os
 import socket
 import time, datetime
 from os.path import join, getsize
+from hdfs.hfile import Hfile
+from hdfs.hfilesystem import Hfilesystem
 
 MASTER_PORT = 9999
 SLAVE_PORT = MASTER_PORT + 1
@@ -37,15 +39,15 @@ class LFS(object):
     pass
   
   def mkdir(self, dirname):
-    if not os.path.exists(dirname):
+    if not self.exists(dirname):
       os.mkdir(dirname)
   
   def rmdir(self, dirname):
-    if os.path.exists(dirname):
+    if self.exists(dirname):
       os.rmdir(dirname)
       
   def rm_rf(self, d):
-    if os.path.exists(d):
+    if self.exists(d):
       for path in (os.path.join(d,f) for f in os.listdir(d)):
         if os.path.isdir(path):
           self.rm_rf(path)
@@ -54,10 +56,10 @@ class LFS(object):
       os.rmdir(d)
     
   def is_done(self, dirname):
-    return os.path.exists(os.path.join(dirname, DONE_TAG))
+    return self.exists(os.path.join(dirname, DONE_TAG))
   
   def get_subdirs(self, dirname, checkdone = False):
-    if not os.path.exists(dirname):
+    if not self.exists(dirname):
       return []
     return [sdir for sdir in os.listdir(dirname) if os.path.isdir(os.path.join(dirname, sdir)) 
             if not checkdone or self.is_done(os.path.join(dirname, sdir))]
@@ -67,15 +69,15 @@ class LFS(object):
     
   def get_unfinished_subdirs(self, dirname, jobname = '', checkdone = False):
     return [sdir for sdir in self.get_subdirs(dirname, checkdone) if
-               not os.path.exists(os.path.join(dirname, sdir, jobname + FINISHED_TAG))]
+               not self.exists(os.path.join(dirname, sdir, jobname + FINISHED_TAG))]
     
   def get_buffered_subdirs(self, dirname, jobname = '', checkdone = False):
     return [sdir for sdir in self.get_subdirs(dirname, checkdone = False) if
-               not os.path.exists(os.path.join(dirname, sdir, jobname + FINISHED_TAG))
-               and not os.path.exists(os.path.join(dirname, sdir, jobname + STARTED_TAG))]
+               not self.exists(os.path.join(dirname, sdir, jobname + FINISHED_TAG))
+               and not self.exists(os.path.join(dirname, sdir, jobname + STARTED_TAG))]
     
   def get_subfiles(self, dirname):
-    if not os.path.exists(dirname):
+    if not self.exists(dirname):
       return []
     return [sfile for sfile in os.listdir(dirname) if os.path.isfile(os.path.join(dirname, sfile)) ]
   
@@ -98,7 +100,7 @@ class LFS(object):
   # recursive
   def get_dir_size(self, dirname):
     size = 0L
-    if os.path.exists(dirname):
+    if self.exists(dirname):
       for root, dirs, files in os.walk(dirname):
         size += sum([getsize(join(root, name)) for name in files])
     return size
@@ -106,7 +108,7 @@ class LFS(object):
   # recursive
   def get_unfinished_dir_size(self, dirname, jobname = ''):
     size = 0L
-    if os.path.exists(dirname):
+    if self.exists(dirname):
       for root, dirs, files in os.walk(dirname):
         if not (jobname + FINISHED_TAG) in dirs: 
           size += sum([getsize(join(root, name)) for name in files])
@@ -114,63 +116,66 @@ class LFS(object):
   
   def get_buffered_dir_size(self, dirname, jobname = ''):
     size = 0L
-    if os.path.exists(dirname):
+    if self.exists(dirname):
       for root, dirs, files in os.walk(dirname):
         if (not (jobname + FINISHED_TAG) in dirs) and (not (jobname + STARTED_TAG) in dirs): 
           size += sum([getsize(join(root, name)) for name in files])
     return size
   
   def exists(self, pathname):
-    return os.path.exists(pathname)
+    return self.exists(pathname)
   
   
 class DFS(object):
-  def __init__(self):
-    pass
+  def __init__(self, hostname='speedy', port=8020):
+    self.hfs = Hfilesystem(hostname, port)
+    self.urlhead = 'hdfs://'  + hostname + ':' + str(port)
+    self.urlheadlen = len(self.urlhead)
   
   def mkdir(self, dirname):
-    if not os.path.exists(dirname):
-      os.mkdir(dirname)
+    if not self.exists(dirname):
+      self.hfs.mkdir(dirname)
   
   def rmdir(self, dirname):
-    if os.path.exists(dirname):
-      os.rmdir(dirname)
+    if self.exists(dirname):
+      self.hfs(dirname)
       
   def rm_rf(self, d):
-    if os.path.exists(d):
-      for path in (os.path.join(d,f) for f in os.listdir(d)):
-        if os.path.isdir(path):
-          self.rm_rf(path)
-        else:
-          os.unlink(path)
-      os.rmdir(d)
+    self.rmdir(d)
     
   def is_done(self, dirname):
-    return os.path.exists(os.path.join(dirname, DONE_TAG))
+    return self.exists(os.path.join(dirname, DONE_TAG))
   
   def get_subdirs(self, dirname, checkdone = False):
-    return [sdir for sdir in os.listdir(dirname) if os.path.isdir(os.path.join(dirname, sdir)) 
+    if not self.exists(dirname):
+      return []
+    return [sdir[sdir.rindex['/']+1:] for sdir in self.hfs.listdir(dirname) if self.hfs.isDir(os.path.join(dirname, sdir)) 
+            if not checkdone or self.is_done(os.path.join(dirname, sdir))]
+  
+  def get_abs_subdirs(self, dirname, checkdone = False):
+    if not self.exists(dirname):
+      return []
+    return [sdir[self.urlheadlen:] for sdir in self.hfs.listdir(dirname) if self.hfs.isDir(os.path.join(dirname, sdir)) 
             if not checkdone or self.is_done(os.path.join(dirname, sdir))]
     
-  def get_abs_subdirs(self, dirname, checkdone = False):
-    return [os.path.join(dirname, sdir) for sdir in os.listdir(dirname) if os.path.isdir(os.path.join(dirname, sdir)) 
-            if not checkdone or self.is_done(os.path.join(dirname, sdir))]    
-  
   def get_unfinished_subdirs(self, dirname, jobname = '', checkdone = False):
-    return [sdir for sdir in self.get_subdir_num(dirname, checkdone) and
-               not os.path.exists(os.path.join(dirname, sdir, jobname + FINISHED_TAG))]
+    return [sdir for sdir in self.get_subdirs(dirname, checkdone) if
+               not self.exists(os.path.join(dirname, sdir, jobname + FINISHED_TAG))]
     
   def get_buffered_subdirs(self, dirname, jobname = '', checkdone = False):
-    return [sdir for sdir in self.get_subdir_num(dirname, checkdone = False) and
-               not os.path.exists(os.path.join(dirname, sdir, jobname + FINISHED_TAG))
-               and not os.path.exists(os.path.join(dirname, sdir, jobname + STARTED_TAG))]
+    return [sdir for sdir in self.get_subdirs(dirname, checkdone = False) if
+               not self.exists(os.path.join(dirname, sdir, jobname + FINISHED_TAG))
+               and not self.exists(os.path.join(dirname, sdir, jobname + STARTED_TAG))]
     
   def get_subfiles(self, dirname):
+    if not self.exists(dirname):
+      return []
     return [sfile for sfile in os.listdir(dirname) if os.path.isfile(os.path.join(dirname, sfile)) ]
   
   def get_abs_subfiles(self, dirname):
-    return [os.path.join(dirname, sfile) for sfile in os.listdir(dirname) if os.path.isfile(os.path.join(dirname, sfile)) ]
+    return [os.path.join(dirname, sfile) for sfile in self.get_subfiles(dirname) ]
   
+
   def get_subdir_num(self, dirname):
     return len(self.get_subdirs(dirname))
   
@@ -186,27 +191,32 @@ class DFS(object):
   # recursive
   def get_dir_size(self, dirname):
     size = 0L
-    for root, dirs, files in os.walk(dirname):
-      size += sum([getsize(join(root, name)) for name in files])
+    if self.exists(dirname):
+      for root, dirs, files in os.walk(dirname):
+        size += sum([getsize(join(root, name)) for name in files])
     return size
   
   # recursive
   def get_unfinished_dir_size(self, dirname, jobname = ''):
     size = 0L
-    for root, dirs, files in os.walk(dirname):
-      if not (jobname + FINISHED_TAG) in dirs: 
-        size += sum([getsize(join(root, name)) for name in files])
+    if self.exists(dirname):
+      for root, dirs, files in os.walk(dirname):
+        if not (jobname + FINISHED_TAG) in dirs: 
+          size += sum([getsize(join(root, name)) for name in files])
     return size
   
   def get_buffered_dir_size(self, dirname, jobname = ''):
     size = 0L
-    for root, dirs, files in os.walk(dirname):
-      if (not (jobname + FINISHED_TAG) in dirs) and (not (jobname + STARTED_TAG) in dirs): 
-        size += sum([getsize(join(root, name)) for name in files])
+    if self.exists(dirname):
+      for root, dirs, files in os.walk(dirname):
+        if (not (jobname + FINISHED_TAG) in dirs) and (not (jobname + STARTED_TAG) in dirs): 
+          size += sum([getsize(join(root, name)) for name in files])
     return size
   
   def exists(self, pathname):
-    return os.path.exists(pathname)
+    return self.hfs.exists(pathname)
+  
+  
   
 def gettime(timetpl):
   return time.mktime(datetime.datetime(*timetpl).timetuple()) 
